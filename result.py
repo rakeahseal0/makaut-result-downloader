0261a4f1bef7f35610466044ddaee6d5ad8ac9cd
rakeahseal0 <rakeshseal0@gmail.com>
2018-02-04 21:08:16 +0530

    Create result.py

---
+++ result.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from  selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import urllib
+from PIL import Image
+from pytesseract import image_to_string
+import time
+
+
+
+roll1=input('Enter Starting roll Number: ') #Starting Roll Number
+numberOfIt=input('How many result: ')  #Number of result to be downloaded from starting roll '1' for single result
+save=1
+
+
+#Add a random valid path for captcha location
+path='E:/'
+
+#Add a path in which results will be downloaded and saved
+savePath='E:/makaut/'
+
+roll=int(roll1)
+
+driver=webdriver.Firefox()
+
+driver.set_window_size(1500,600)  #do not Decrease window size it may give you captcha error
+for _ in range(0,int(numberOfIt)):
+	driver.get('http://makautexam.net/GradeCardGenerateAndPrint.aspx?Type=UG')
+	driver.find_element_by_xpath("//input[@id='txtrollcode']").send_keys(roll)
+	driver.find_element_by_xpath("//form[@id='form1']").click()
+	time.sleep(2)
+	driver.find_element_by_xpath("//option[@value='03']").click()
+	srcc=driver.find_element_by_xpath("//img[@alt='Captcha']").get_attribute('src')
+	urllib.request.urlretrieve(srcc,'E:/1.png')
+	a=Image.open(path+'1.png')    #saving captcha image
+	img_info=a.info
+	#a.save('E:/hh.png',**img_info)
+	b=a.convert('RGBA')   
+	num=image_to_string(b)  #using OCR for getting String from text 
+	num=num.replace(':',' ') 
+	captcha=eval(num)
+	print(captcha)
+	driver.find_element_by_xpath("//input[@id='txtCaptcha']").send_keys(captcha)
+	driver.find_element_by_xpath("//input[@class='btn btn-success']").click()
+	time.sleep(1)
+	driver.switch_to_alert().accept()
+	time.sleep(1)
+	driver.save_screenshot(savePath+str(save)+'.png')  #saving result as screenshot
+	roll=roll+1
+	save=save+1
+print('Downloaded in'+savePath)
+driver.quit()
+
+
+
+
+
